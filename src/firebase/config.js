2b28bd131cde4a725a574b4da8f06a0631b914bf
Kenenisaboru <kenenisaboru998@gmail.com>
2026-08-04 05:38:07 -0700

    Add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration Module for LifeLink
+// Note: Can be replaced with your custom Firebase project keys or demo config.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDemoKeyLifeLink2026HackathonExpo",
+  authDomain: "lifelink-hackathon.firebaseapp.com",
+  projectId: "lifelink-hackathon",
+  storageBucket: "lifelink-hackathon.appspot.com",
+  messagingSenderId: "891274910293",
+  appId: "1:891274910293:web:a0b1c2d3e4f5a6b7c8d9e0"
+};
