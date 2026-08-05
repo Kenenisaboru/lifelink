f9c32b2fc9d1649eb6a9cd3da637d098dd40d838
Kenenisaboru <kenenisaboru998@gmail.com>
2026-08-05 01:18:34 -0700

    chore: update dependencies and Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@stores': './src/stores',
+            '@services': './src/services',
+            '@firebase': './src/firebase',
+            '@utils': './src/utils',
+            '@types': './src/types',
+            '@theme': './src/theme',
+            '@navigation': './src/navigation',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin', // Must be last
+    ],
   };
 };
